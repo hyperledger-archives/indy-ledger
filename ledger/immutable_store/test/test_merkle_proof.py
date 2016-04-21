1ebba82359fb5d1c58243eef110640c13ce23921
Lovesh <lovesh@evernym.us>
2016-04-21 18:20:27 +0530

    preventing excessive output in test

--- ledger/immutable_store/test/test_merkle_proof.py
+++ ledger/immutable_store/test/test_merkle_proof.py
@@ -133,38 +133,38 @@ def testFullMerkleTree(hasherAndFullMerkleTree):
     for d in range(100000):
         data = str(d+1).encode()
         m.append(data)
-        # if d % 1000 == 0:
-        show(h, m, data)
-        start = time.perf_counter()
-        inc_proof = m.inclusion_proof(d, d + 1)
-
-        print("Time taken to generate leaf inclusion proof in tree of size {} "
-              "is {}".format(d + 1, time.perf_counter() - start))
-        sth = STH(d+1, m.root_hash())
-
-        print("STH : {}".format(sth))
-        print("audit_path : {}".format(inc_proof))
-        print("size of audit_path : {}".format(len(inc_proof)))
-
-        merkleTreeHead = base64.b64encode(m._calc_mth(0, d+1))
-        if d > 7:
-            print(base64.b64encode(m._calc_mth(0, d-6)).decode())
-        print("merkle_tree_head : {}".format(merkleTreeHead))
-
-        rootHash = base64.b64encode(m.root_hash())
-        print("root_hash : {}".format(rootHash == merkleTreeHead))
-        assert merkleTreeHead == rootHash
-
-        start = time.perf_counter()
-        assert verifier.verify_leaf_inclusion(data, d, [base64.b64decode(
-            a.encode()) for a in inc_proof], sth)
-        print("Time taken to verify leaf inclusion in tree of size {} from "
-              "audit path is {}".format(d+1, time.perf_counter()-start))
-
-        if d in range(17001, 17002, 17003):
+        if d % 1000 == 0:
+            show(h, m, data)
+            start = time.perf_counter()
             inc_proof = m.inclusion_proof(d, d + 1)
+
+            print("Time taken to generate leaf inclusion proof in tree of size {} "
+                  "is {}".format(d + 1, time.perf_counter() - start))
+            sth = STH(d+1, m.root_hash())
+
+            print("STH : {}".format(sth))
             print("audit_path : {}".format(inc_proof))
             print("size of audit_path : {}".format(len(inc_proof)))
+
+            merkleTreeHead = base64.b64encode(m._calc_mth(0, d+1))
+            if d > 7:
+                print(base64.b64encode(m._calc_mth(0, d-6)).decode())
+            print("merkle_tree_head : {}".format(merkleTreeHead))
+
+            rootHash = base64.b64encode(m.root_hash())
+            print("root_hash : {}".format(rootHash == merkleTreeHead))
+            assert merkleTreeHead == rootHash
+
+            start = time.perf_counter()
+            assert verifier.verify_leaf_inclusion(data, d, [base64.b64decode(
+                a.encode()) for a in inc_proof], sth)
+            print("Time taken to verify leaf inclusion in tree of size {} from "
+                  "audit path is {}".format(d+1, time.perf_counter()-start))
+
+            if d in range(17001, 17002, 17003):
+                inc_proof = m.inclusion_proof(d, d + 1)
+                print("audit_path : {}".format(inc_proof))
+                print("size of audit_path : {}".format(len(inc_proof)))
     print("Test ends in {}".format(time.perf_counter() - testStart))
 
 
@@ -174,14 +174,14 @@ def testCompactMerkleTree(hasherAndCompactMerkleTree):
     for d in range(100000):
         data = str(d + 1).encode()
         auditPath = m.append(data)
-        # if d % 1000 == 0:
-        show(h, m, data)
-        print("audit path is {}".format([hexlify(ap) for ap in auditPath]))
-        print("audit path length is {}".format(verifier.audit_path_length(d, d+1)))
-        print("audit path calculated length is {}".format(
-            len(auditPath)))
-        calculated_root_hash = verifier._calculate_root_hash_from_audit_path(
-            h.hash_leaf(data), d, auditPath[:], d+1)
-        print("calculated root hash is {}".format(hexlify(calculated_root_hash)))
-        sth = STH(d+1, m.root_hash())
-        verifier.verify_leaf_inclusion(data, d, auditPath, sth)
+        if d % 1000 == 0:
+            show(h, m, data)
+            print("audit path is {}".format([hexlify(ap) for ap in auditPath]))
+            print("audit path length is {}".format(verifier.audit_path_length(d, d+1)))
+            print("audit path calculated length is {}".format(
+                len(auditPath)))
+            calculated_root_hash = verifier._calculate_root_hash_from_audit_path(
+                h.hash_leaf(data), d, auditPath[:], d+1)
+            print("calculated root hash is {}".format(hexlify(calculated_root_hash)))
+            sth = STH(d+1, m.root_hash())
+            verifier.verify_leaf_inclusion(data, d, auditPath, sth)
