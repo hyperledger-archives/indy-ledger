6b13f4a73224329b7ea4f43eb469bd565292c02e
Rajesh Kalaria <rajesh.kalaria@gmail.com>
2016-10-14 12:49:19 +0530

    advance version number to push to pypi

--- ledger/__metadata__.py
+++ ledger/__metadata__.py
@@ -1,7 +1,7 @@
 """
 Ledger package metadata
 """
-__version_info__ = (0, 0, 27)
+__version_info__ = (0, 0, 28)
 __version__ = '{}.{}.{}'.format(*__version_info__)
 __author__ = "Evernym, Inc."
 __license__ = "Apache 2.0"
