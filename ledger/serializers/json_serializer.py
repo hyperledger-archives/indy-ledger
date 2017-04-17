0e11f997df7b1df0f69da3bff620599c257aec1b
Lovesh <lovesh.harchandani@evernym.com>
2017-04-17 19:26:33 +0530

    adding test for serialisation and  handling absence of ujson

--- ledger/serializers/json_serializer.py
+++ ledger/serializers/json_serializer.py
@@ -1,26 +1,45 @@
+# Consider using bson or ubjson for serializing json
+
+
 import base64
-import ujson as json
-from ujson import encode as uencode
 from typing import Dict
 
 from ledger.serializers.mapping_serializer import MappingSerializer
 
 
-# Consider using bson or ubjson for serializing json
+try:
+    import ujson as json
+    from ujson import encode as uencode
 
 
-# class OrderedJsonEncoder(json.JSONEncoder):
-#     def __init__(self, *args, **kwargs):
-#         kwargs['ensure_ascii'] = False
-#         kwargs['sort_keys'] = True
-#         kwargs['separators'] = (',', ':')
-#         super().__init__(*args, **kwargs)
-#
-#     def encode(self, o):
-#         if isinstance(o, (bytes, bytearray)):
-#             return '"{}"'.format(base64.b64encode(o).decode("utf-8"))
-#         else:
-#             return json.JSONEncoder.encode(self, o)
+    class UJsonEncoder:
+        @staticmethod
+        def encode(o):
+            if isinstance(o, (bytes, bytearray)):
+                return '"{}"'.format(base64.b64encode(o).decode("utf-8"))
+            else:
+                return uencode(o, sort_keys=True)
+
+
+    JsonEncoder = UJsonEncoder()
+
+except ImportError:
+    import json
+
+    class OrderedJsonEncoder(json.JSONEncoder):
+        def __init__(self, *args, **kwargs):
+            kwargs['ensure_ascii'] = False
+            kwargs['sort_keys'] = True
+            kwargs['separators'] = (',', ':')
+            super().__init__(*args, **kwargs)
+
+        def encode(self, o):
+            if isinstance(o, (bytes, bytearray)):
+                return '"{}"'.format(base64.b64encode(o).decode("utf-8"))
+            else:
+                return json.JSONEncoder.encode(self, o)
+
+    JsonEncoder = OrderedJsonEncoder()
 
 
 class JsonSerializer(MappingSerializer):
@@ -28,20 +47,11 @@ class JsonSerializer(MappingSerializer):
     Class to convert a mapping to json with keys ordered in lexicographical
     order
     """
-    # jsonEncoder = OrderedJsonEncoder()
-
-    @staticmethod
-    def _encode(o):
-        if isinstance(o, (bytes, bytearray)):
-            return '"{}"'.format(base64.b64encode(o).decode("utf-8"))
-        else:
-            return uencode(o, sort_keys=True)
 
     # The `fields` argument is kept to conform to the interface, its not
     # need in this method
     def serialize(self, data: Dict, fields=None, toBytes=True):
-        # encoded = self.jsonEncoder.encode(data)
-        encoded = self._encode(data)
+        encoded = JsonEncoder.encode(data)
         if toBytes:
             encoded = encoded.encode()
         return encoded
