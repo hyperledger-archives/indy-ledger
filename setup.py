67238341e3537556e72e727eddd6b2b37bf7cd61
Lovesh <lovesh.harchandani@evernym.com>
2017-04-18 13:39:47 +0530

    specifying ujson version

--- setup.py
+++ setup.py
@@ -43,7 +43,7 @@ setup(
     author_email='dev@evernym.us',
     license=__license__,
     keywords='Ledger Merkle',
-    install_requires=['ujson'],
+    install_requires=['ujson==1.33'],
     setup_requires=['pytest-runner', ],
     tests_require=['pytest', ],
     packages=find_packages(exclude=['test', 'test.*',
